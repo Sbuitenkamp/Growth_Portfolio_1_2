0b474dffe7fa434a24cc3928717de996e9765e62
sbuitenkamp <buitenkamp.developer@gmail.com>
2020-10-08 20:02:24 +0200

    next big thing

---
+++ README.txt
@@ -0,0 +1,8 @@
+Om dit portfolio te bekijken open het bestand index.html in een van de volgende browsers:
+    - Google Chrome
+    - Firefox
+    - Safari
+    - Microsoft Edge
+    - Opera
+
+Daarna is het hele portfolio te navigeren via de browser.
--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,11 @@
 window.onload = () => {
-    const base = 0.10;
+    const base = 0.0;
     const background = document.getElementById("background");
     const body = document.getElementById("content");
     background.style.opacity = base.toString();
 
     body.addEventListener('scroll', () => {
-        const opacity = "0." + body.scrollTop.toString().padStart(3, "0");
+        const opacity = "0." + body.scrollTop.toString().padStart(4, "0");
         if (opacity > base) background.style.opacity = opacity;
-    }, { passive: true });
+    });
 };
